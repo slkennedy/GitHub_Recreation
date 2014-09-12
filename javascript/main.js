9cbb2f3373e4450b8d303f624658e10670ee4e20
slkennedy <slkennedy@gmail.com>
2014-09-11 21:15:07 -0400

    Completed Repository Inclusion

--- javascript/main.js
+++ javascript/main.js
@@ -1,34 +1,56 @@
 
 function reusableTemplate(templateId, container, model) {
-var templateFunction = _.template($('#' + templateId).text());
-var renderedTemplate = templateFunction(model);
-$(container).append(reusableTemplate);
+var templateFunction = _.template($('#'+ templateId).text());
 
+var renderedTemplate = templateFunction(model);
+console.log(renderedTemplate)
+$(container).append(renderedTemplate);
+}
 
 $.getJSON("https://api.github.com/users/slkennedy").done(function(user){
-
-reuableTemplate('templateMenuBar', '.menuRight', user);
-
-return{
-  avatar: user.avatar,
-  name: user.name,
-  login: user.login,
-  avatarUrl: user.avatar_url,
-  email: user.email,
-  followers: user.followers,
-  followersUrl: user.follower_url,
-  following: user.following,
-  followingUrl: user.following_url,
-  location: user.location
-};
+  var userData = {
+    avatar_url: user.avatar_url,
+    name: user.name,
+    login: user.login,
+    email: user.email,
+    followers: user.followers,
+    followersUrl: user.follower_url,
+    following: user.following,
+    followingUrl: user.following_url,
+    location: user.location
+  };
+  reusableTemplate('templateMenuBar', '.menuRight', userData);
 });
 
-$.getJson("https://api.github.com/users/slkennedy/repos").done(function(repos){
-  console.log(repos);
+$.getJSON("https://api.github.com/users/slkennedy/repos").done(function(repos){
+  var repoData = _.map(repos, function(repo){
+  return{
+    repoName: repo.name,
+    repoDescription: repo.description,
+    forks: repo.forks,
+    forksCount: repo.forks_count,
+    forksUrl: repo.forks_url,
+    gitUrl: repo.git_url,
+    stargazersCount: repo.stargazers_count,
+    stargazersUrl: repo.stargazers_url,
+    updated: repo.updated_at,
+    language: repo.language};
+  });
+console.log(repoData);
+
+
+_.each(repoData, function(output){
+reusableTemplate('templateMainContent', '.mainContentFeature', output);
 });
-$.getJson("https://api.github.com/users/slkennedy/starred").done(function(starred){
-  console.log(starred);
+
 });
-$.getJson("https://api.github.com/users/slkennedy/orgs").done(function(orgs){
-  console.log(orgs);
-});
+
+
+
+
+// $.getJson("https://api.github.com/users/slkennedy/starred").done(function(starred){
+//   console.log(starred);
+// });
+// $.getJson("https://api.github.com/users/slkennedy/orgs").done(function(orgs){
+//   console.log(orgs);
+// });
