0ef73ade792e359970d997e9bf42afc2e6a054ef
slkennedy <slkennedy@gmail.com>
2014-09-13 00:13:23 -0400

    Cont'd adjusting styling and added icon fonts

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,20 @@
 
 function reusableTemplate(templateId, container, model) {
 var templateFunction = _.template($('#'+ templateId).text());
-
 var renderedTemplate = templateFunction(model);
 $(container).append(renderedTemplate);
 }
 
 $.getJSON("https://api.github.com/users/slkennedy").done(function(user){
+
   var userData = {
     avatarUrl: user.avatar_url,
     login: user.login,
   };
-  reusableTemplate('templateMenuBar', '.menuRight', userData);
+  reusableTemplate('templateMenuBar', '.menurtuser', userData);
+});
+
+$.getJSON("https://api.github.com/users/slkennedy").done(function(user){
 
   var sidebarData = {
     avatarUrl: user.avatar_url,
@@ -25,8 +28,11 @@ $.getJSON("https://api.github.com/users/slkennedy").done(function(user){
     followingUrl: user.following_url,
     location: user.location
   };
-  reusableTemplate('templateSideBar', '.sideBar', sidebarData);
-
+  reusableTemplate('templateSideBar', '.sideBarTop', sidebarData);
+  $.getJSON('https://api.github.com/users/slkennedy/starred').done(function(stars) {
+    sidebardata.starred = stars.length;
+    renderTemplate('templateSideBar', '.sideBarTop', sidebarData);
+  });
 });
 
 $.getJSON("https://api.github.com/users/slkennedy/repos").done(function(repos){
@@ -50,10 +56,6 @@ reusableTemplate('templateMainContent', '.mainContentFeature', output);
 
 });
 
-// $.getJSON("https://api.github.com/users/slkennedy/starred").done(function(starred){
-// console.log(starred);
-// });
-
 $.getJSON("https://api.github.com/users/slkennedy/orgs").done(function(orgs){
 var orgData = _.map(orgs, function(org){
 return{
@@ -63,6 +65,6 @@ return{
 });
 
 _.each(orgData, function(output){
-reusableTemplate('templateSideBarOrgs', '.sideBar', output);
+reusableTemplate('templateSideBarOrgs', '.sideBarBottom', output);
 });
 });
